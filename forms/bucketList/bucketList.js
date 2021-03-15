9f0a3cbc994ea63cfb91feb04f570caea4ec66db
Elliot R Hill <erh81010@creighton.edu>
2021-03-15 16:29:27 -0500

    added bucketList

---
+++ forms/bucketList/bucketList.js
@@ -0,0 +1,7 @@
+let bucketList = [ ]
+alert('Do you want to see your bucket list?')
+if (alert == true)
+    console.log(bucketList)
+else
+    let userBucketList = prompt('Enter an item into your bucket list')
+    bucketList.push(userBucketList)
